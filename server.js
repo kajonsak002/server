ca6018a42b7c932a9beaf323d44ff301bdd5aca5
kajonsak002 <651463002@crru.ac.th>
2023-04-26 12:06:36 +0700

    Server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 require("express-async-errors");
 const express = require("express"); // ดึง Library Express มาใช้งาน
 const app = express(); //เรียกใช้งาน
+const path = require("path");
+const cookieParser = require("cookie-parser");
 const cors = require("cors"); //กำหนด http ที่เข้ามา
 const corsOptions = require("./src/config/corsOption");
 const mongoose = require("mongoose");
@@ -10,16 +12,21 @@ const connectDB = require("./src/config/dbConn");
 connectDB();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
+app.use(cookieParser());
+
+app.use("/", express.static(path.join(__dirname, "public")));
+
+console.log(__dirname);
 
 // Mogoose setup //
 const port = process.env.PORT || 5001;
 
-mongoose.connection.once("opne", () => {
+mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB");
   app.listen(port, () => console.log(`server is runing with port ${port}`));
 });
 
-mongoose.connection.on('error', (err) => {
-    console.log(err);
-})
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
